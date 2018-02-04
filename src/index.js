a27b06c77d3229f8e9ed8bf034a83af5534005c4
mutasem <mutasem.halawani@gmail.com>
2018-02-04 14:43:09 +0200

    edit homepage text trimming

--- src/index.js
+++ src/index.js
@@ -2,14 +2,65 @@
 
 import './assets/styles/styles.scss';
 import './assets/styles/home.scss';
-import HelloWorld from './assets/js/hello-world';
 
 import MenuToggle from './assets/js/common/mobile-menu';
 
 require('html-loader!./templates/index.html');
 
-let indexPage = new HelloWorld('Hello from index page!');
-console.log(indexPage.sayHello());
+
+const desktopText = `
+    GLE is an initiative of the Rothschild Foundation Hanadiv
+    Europe and the National Library of Israel to create opportunities
+    for European scholars, library and museum professionals, educators
+    and community activists working within Jewish settings to develop an on-going,
+    mutually beneficial relationship with the NLI and to engage with the NLI's
+    diverse and extensive collections.
+`;
+
+function trimText() {
+    $(".header-text").each(function(i){
+        var len = $(this).text().trim().length;
+        if ( len > 145 ){
+            $(this).text($(this).text().substr(0,145) + '...')
+                .append($(`
+                    <span id="read-more-container">
+                        <a href="" id="read-more-link">read more</a>
+                    </span>
+                `));
+        }
+    });
+}
+
+$(document).ready(function (){
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        trimText();
+    }
+});
+
+$(window).on('resize', function() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        trimText();
+    } else {
+        $(".header-text").text(desktopText);
+    }
+});
+
+$('body').on('click','#read-more-link', function(e) {
+    e.preventDefault();
+    
+    $(".header-text").text(desktopText)
+        .append($(`
+            <span id="read-less-container">
+                <a href="" id="read-less-link">read less</a>
+            </span>
+        `));
+});
+
+$('body').on('click','#read-less-link', function(e) {
+    e.preventDefault();
+    trimText();
+});
+
 
 
   

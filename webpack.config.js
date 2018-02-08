42bf5c07b693858835482e56ca83fcb9a45e7d91
mutasem <mutasem.halawani@gmail.com>
2018-02-08 09:36:17 +0200

    change build features

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,9 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'assets/js/[name].bundle.js'
     },
-    devtool: "source-map",
+    // devtool: "source-map",
     module: {
-        rules: [
+        rules: [ 
             {
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src'),
@@ -44,26 +44,39 @@ const config = {
                 }]
             },
             {
-                test: /\.scss$/,
-                loader: extractCSS.extract([
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: postCSSOptions
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ])
+                test   : /\.css$/,
+                loaders: ['style-loader', 'css-loader', 'resolve-url-loader']
             },
+            {
+                test   : /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+            },
+            // {
+            //     test: /\.scss$/,
+            //     loader: extractCSS.extract([
+            //         {
+            //             loader: 'resolve-url-loader',
+            //         },
+                    
+            //         {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 sourceMap: true
+            //             }
+            //         },
+
+            //         {
+            //             loader: 'postcss-loader',
+            //             options: postCSSOptions
+            //         },
+            //         {
+            //             loader: 'sass-loader',
+            //             options: {
+            //                 sourceMap: true
+            //             }
+            //         }
+            //     ]),
+            // },
 
             {
                 test: /\.(png|jpg)$/,
@@ -94,8 +107,17 @@ const config = {
             },
             {
                 test: /\.(woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=100000'
-            }
+                // loader: 'url-loader?name=assets/fontslimit=100000'
+                loader: 'url-loader?limit=100000&name=./assets/fonts/[hash].[ext]'
+            },
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|svg)$/,
+            //     loader: fontLocal
+            // },
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|svg)$/,
+            //     loader: 'file-loader'
+            // }
         ]
     },
     plugins: [

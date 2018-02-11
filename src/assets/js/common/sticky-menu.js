36d3361792913d3cfe1d5bbff01f2fec1aa1b3a5
mutasem <mutasem.halawani@gmail.com>
2018-02-11 11:15:02 +0200

    edit gradients

--- src/assets/js/common/sticky-menu.js
+++ src/assets/js/common/sticky-menu.js
@@ -1,67 +1,16 @@
 const StickyMenu = (() => {
 
-    // var lastScrollTop = 0;
-    // var delta = 5;
-    // var navbarHeight = $('.sticky-menu-wrapper').outerHeight();
-
-    // $(window).scroll(function () {
-    //     clearTimeout($.data(this, 'scrollTimer'));
-    //     $.data(this, 'scrollTimer', setTimeout(function () {
-    //         console.log('scrolling'); 
-    //         var st = $(window).scrollTop();
-    //         if (Math.abs(lastScrollTop - st) <= delta)
-    //             return;
-
-    //         if (st > lastScrollTop && st > navbarHeight){
-    //             // downscroll code
-    //             $('.sticky-menu-wrapper').removeClass('nav-down')
-    //             .addClass('nav-up');
-    //         } else {
-    //             // upscroll code
-    //             if (st + $(window).height() < $(document).height()) {
-    //                 $('.sticky-menu-wrapper').removeClass('nav-up')
-    //                     .addClass('nav-down');
-    //             }
-    //         }
-    //         lastScrollTop = st;
-    //     }, 50));
-    // });
-
-
-    // *************************************************************
-
-
-    // var lastScrollTop = 0;
-    // $(window).scroll(function(event){
-    //    var st = $(this).scrollTop();
-    //    if (st > lastScrollTop){
-    //        // downscroll code
-    //        $('.sticky-menu-wrapper').removeClass('nav-down')
-    //             .addClass('nav-up');
-    //     } else {
-    //         // upscroll code
-    //         $('.sticky-menu-wrapper').removeClass('nav-up')
-    //             .addClass('nav-down');
-    //    }
-    //    lastScrollTop = st;
-    // });
-
-    // *************************************************************
-    
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('.sticky-menu-wrapper').outerHeight();
-    
-    // console.log(navbarHeight);
+    let didScroll;
+    let lastScrollTop = 0;
+    let delta = 5;
+    let navbarHeight = $('.sticky-menu-wrapper').outerHeight();
+    let headerHeight = $('header').outerHeight();
     
     $(window).scroll(function(e) {
         didScroll = true;
     });
     
     setInterval(function(){
-
-        // console.log(didScroll);
         if (didScroll) {
             hasScrolled();
             didScroll = false;
@@ -70,20 +19,29 @@ const StickyMenu = (() => {
     
     function hasScrolled() {
 
-        var st = $(window).scrollTop();
+        let st = $(window).scrollTop();
         if(Math.abs(lastScrollTop - st) <= delta)
         return;
-
+        
         if (st > lastScrollTop && st > navbarHeight){
             // Scroll Down
             $('.sticky-menu-wrapper').removeClass('nav-down')
-                .addClass('nav-up');
+            .addClass('nav-up');
         } else {
             // Scroll Up
             if (st + $(window).height() < $(document).height()) {
                 $('.sticky-menu-wrapper').removeClass('nav-up')
                     .addClass('nav-down');
             }
+
+            // Change Menu color depending on Window ScrollTop
+            if ($(window).scrollTop() > headerHeight) {
+                $('.sticky-menu-wrapper').addClass('sticky-menu-black');
+                console.log('change color now');
+            } else {
+                $('.sticky-menu-wrapper').removeClass('sticky-menu-black');
+            }
+            
         }
 
         lastScrollTop = st;

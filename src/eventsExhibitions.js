4d655d901fd46a61e6294c31e4c5a1959dbc2ca9
mutasem <mutasem.halawani@gmail.com>
2018-02-11 18:01:11 +0200

    fix events photo lazyload

--- src/eventsExhibitions.js
+++ src/eventsExhibitions.js
@@ -2,6 +2,7 @@ import './assets/styles/eventsExhibitions.scss';
 import './assets/styles/styles.scss';
 
 import 'lazysizes';
+
 import { MenuToggle, StickyMenu, FooterBtn } from './assets/js/common';
 
 require('html-loader!./templates/eventsExhibitions.html');
